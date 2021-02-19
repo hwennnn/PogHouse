86d6602450b6efdebcb145a0a7497b3d5bca8b24
hwen <whman63@gmail.com>
2021-02-19 16:57:42 +0800

    add firebase to ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app/sign_in/sign_in_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
