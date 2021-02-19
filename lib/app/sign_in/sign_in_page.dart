72d6564ede099636e7366cb1a3c41eec3ecd643c
hwen <whman63@gmail.com>
2021-02-19 15:19:29 +0800

    sign in page UI

---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:poghouse/app/sign_in/social_sign_in_button.dart';
+
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PogHouse'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign in to PogHouse',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(height: 48.0),
+          SocialSignInButton(
+            assetName: 'images/google-logo.png',
+            text: 'Sign in with Google',
+            textColor: Colors.black87,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(height: 8.0),
+          SocialSignInButton(
+            assetName: 'images/facebook-logo.png',
+            text: 'Sign in with Facebook',
+            textColor: Colors.white,
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
